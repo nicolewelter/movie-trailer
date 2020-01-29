4e0a6da821b4f9abd8b95597832b24f7de415f4d
nicolewelter <49258266+nicolewelter@users.noreply.github.com>
2020-01-29 14:35:59 -0800

    Styling Video
    Added color and customization

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,15 @@
 console.log('OK');
 
-const player = new Plyr('#player');
+// Initialize player
+const player = new Plyr( '#player', {
+  
+    // Turn on/off conrols. Array data type
+    controls : [ 'play', 'current-time', 'progress', 'play-large', 'fullscreen'],
+    
+    // Turn on keyboard. Boolean data type
+    keyboard : { focused: true, global: true },
+    
+    // Increase/Decrease seek time. Integer data type
+    seekTime : 30
+  
+  });
